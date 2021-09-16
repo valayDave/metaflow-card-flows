58d916a945f5388ee464b20733e3710b3f415245
Valay Dave <valaygaurang@gmail.com>
2021-09-16 12:49:53 -0700

    Finished data loading for the coveo challenge flow. - Moved code from jacopo's repo;

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-TODO : Document Stuff over here. 
+# Metaflow ML Pipeline With The Coveo Challenge Dataset
+
+- A fork from [this repo](https://github.com/jacopotagliabue/you-dont-need-a-bigger-boat)
+
+
+## Getting Started. 
+
+1. Download the [Dataset](https://www.coveo.com/en/ailabs/sigir-ecom-data-challenge) from the [2021 Coveo Data .Challenge](https://github.com/coveooss/SIGIR-ecom-data-challenge) to your local directory of this repository. 
+2. Install Requirements : `pip install -r requirements.txt`
+3. Run `python s3_upload.py --target-path <PATH-TO-SAVING-PARQUET>`
+4. TODO : Running and Visualizing the results of the `coveo-challenge-flow.py`
+
 
--- coveo-challenge-flow.py
+++ coveo-challenge-flow.py
@@ -1,18 +1,43 @@
 # Todo : Add Flow relating to the coveo challenge flow over here.
-from metaflow import FlowSpec,step,batch
+from metaflow import FlowSpec,step,batch,S3,Parameter,batch,conda
+import os
 
 class CoveoChallengeFlow(FlowSpec):
+    """
+    Take the browsing data and create a new 
+    """
+    num_rows = Parameter('num-rows',default = 1000000,type=int,help='The number of rows from the dataset to use for Training.')
+
+    browsing_path_parquet = Parameter(
+        'browsing-path-parquet',\
+        envvar="BROWSING_PATH_PARQUET",\
+        default=None,type=str,help='Path to the parquet files in S3 for browsing related data in the Coveo Challenge'
+    )
 
     @step
     def start(self):
-        # todo : setup Dataloading and necessary pre-processing of the data
-        # todo : setup Hyperparameters
-        # todo : Find a fast and optimal way to play around with 36M browsing events, 8M search events, 66k Products ;
-        # todo : Fan this into a foreach if necessary
+        assert self.browsing_path_parquet  is not None and 's3://' in self.browsing_path_parquet
+        # setup Dataloading and necessary pre-processing of the data
+        from process_data import process_browsing_train
+        processed_data = process_browsing_train(self.browsing_path_parquet,num_rows=self.num_rows)
+
+        # save as parquet onto S3
+        with S3(run=self) as s3:
+            # save s3 paths in dict
+            self.data_paths = dict()
+            s3_root = s3._s3root            
+            data_path = os.path.join(s3_root, 'browsing_train.parquet')
+            processed_data.to_parquet(path=data_path, engine='pyarrow')
+            self.train_data_path = data_path
+
+        print(self.train_data_path)
         self.next(self.train_model)
 
     @step
     def train_model(self):
+        # todo : setup Hyperparameters
+        # todo : Find a fast and optimal way to play around with 36M browsing events, 8M search events, 66k Products ;
+        # todo : Fan this into a foreach if necessary
         self.next(self.test_model)
 
     @step
